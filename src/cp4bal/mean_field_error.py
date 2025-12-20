88517f8ce1fc097982eef5b24eab43b0ab43c83e
Yuta Ono <ono-yuta116@g.ecc.u-tokyo.ac.jp>
2025-12-20 03:57:02 +0000

    add: error analysis for mean field approximation with CSBM

---
+++ src/cp4bal/mean_field_error.py
@@ -0,0 +1,209 @@
+import traceback
+from itertools import product
+from logging import getLogger
+from pathlib import Path
+
+import numpy as np
+
+from cp4bal.dataset.configs import CommonDatasetConfig, CSBMConfig, DatasetConfig, EdgeProbabilityType
+from cp4bal.dataset.factory import DatasetFactory
+from cp4bal.util.logger import init_stream_logger
+from cp4bal.util.seed import set_seed
+
+logger = getLogger(__name__)
+OUT_DIR = Path(__file__).parent.parent.parent / "out" / "mean_field_error"
+
+
+def main():
+    seed = 42
+    set_seed(seed)
+    init_stream_logger()
+    logger.info("started")
+    logger.info("seed: %d", seed)
+
+    # dataset
+    N = 20  # num nodes
+    K = 2  # num classes
+    SNR = 2.0  # signal to noise ratio
+    D = 4  # expected degree
+    output_dir = OUT_DIR / f"csbm_n{N}_k{K}_snr{SNR}_d{D}_seed{seed}"
+    output_dir.mkdir(parents=True, exist_ok=True)
+    logger.info("output_dir: %s", output_dir.resolve())
+    config = DatasetConfig(
+        common=CommonDatasetConfig(
+            name="csbm",
+            val_size=0,
+            test_size=0,
+        ),
+        detail=CSBMConfig(
+            seed=seed,
+            num_nodes=N,
+            num_classes=K,
+            dim_features=5,
+            feature_sigma=1.0,
+            feature_class_mean_distance=1.0,
+            edge_p_type=EdgeProbabilityType.BY_SNR_AND_DEGREE,
+            p_edge_intra=None,
+            p_edge_inter=None,
+            edge_p_snr=SNR,
+            expected_degree=D,
+        )
+    )
+    base_ds = DatasetFactory.create(config=config)
+    x = base_ds.node_features
+    gt_labels = base_ds.labels
+    np.save(output_dir / "gt_labels.npy", gt_labels.numpy())
+    np.save(output_dir / "node_features.npy", x.numpy())
+    np.save(output_dir / "edge_indices.npy", base_ds.edge_indices)
+
+    # logger.info(f"CSBM Graph created: {base_ds.edge_indices}")
+    logger.info("labels: %s", gt_labels.tolist())
+
+    labeled_nodes = {0, 1, N-2, N-1}  # L
+    Q_nodes = {2, 3, N-3}  # Q
+    unlabeled_nodes = set(range(N)) - labeled_nodes  # U
+
+    q_ids = []
+    R_denominators = []
+    R_numerators = []
+    log_Rs = []
+    mean_field_log_Rs = []
+    logger.info(f"{base_ds.p_edge_inter=}")
+    logger.info(f"{base_ds.p_edge_intra=}")
+
+    mean_field_log_R_const = _calculate_log_R_constant_term(
+        labels=gt_labels,
+        p_inter=base_ds.p_edge_inter,
+        p_intra=base_ds.p_edge_intra,
+        K=K,
+        Q_nodes=list(Q_nodes),
+        L_nodes=list(labeled_nodes),
+        edges=base_ds.edge_indices.clone().cpu().T.tolist(),
+    )
+
+    for q in unlabeled_nodes:
+        q_ids.append(q)
+        T_nodes = unlabeled_nodes - Q_nodes - {q}
+        # S_nodes = labeled_nodes | {q}
+        QT_nodes = Q_nodes | T_nodes
+
+        T_nodes = list(T_nodes)
+        QT_nodes = list(QT_nodes)
+
+        labels_SQ = gt_labels.clone().unsqueeze(0).numpy()
+        labels_S = gt_labels.clone().unsqueeze(0).numpy()
+
+        v_d = 0.0
+        v_n = 0.0
+
+        for allocation in product(list(range(K)), repeat=len(Q_nodes)+len(T_nodes)):
+            allocation = np.array(allocation, dtype=int)
+            labels_SQ[0, T_nodes] = allocation[:len(T_nodes)]  # for P(A | y_S, y_Q)
+            labels_S[0, QT_nodes] = allocation  # for P(A | y_S)
+
+            v_n += np.exp(base_ds.conditional_log_likelihood(
+                y=labels_SQ, x=x, use_adjacency=True, use_features=False, use_non_edges=True
+            )) / (K ** (len(Q_nodes) + len(T_nodes)))
+            v_d += np.exp(base_ds.conditional_log_likelihood(
+                y=labels_S, x=x, use_adjacency=True, use_features=False, use_non_edges=True
+            )) / (K ** (len(Q_nodes) + len(T_nodes)))
+
+        log_R_q = (np.log(v_n) - np.log(v_d)).item()
+        R_numerators.append(v_n.item())
+        R_denominators.append(v_d.item())
+        log_Rs.append(log_R_q)
+
+        mean_field_log_R_q = _calculate_log_R_by_mean_field_approximation(
+            labels=gt_labels,
+            p_inter=base_ds.p_edge_inter,
+            p_intra=base_ds.p_edge_intra,
+            K=K,
+            Q_nodes=list(Q_nodes),
+            i=q,
+            edges=base_ds.edge_indices.clone().cpu().T.tolist(),
+        ) + mean_field_log_R_const
+        mean_field_log_Rs.append(mean_field_log_R_q)
+
+        logger.debug(f"q: {q}, log_R_q: {log_R_q}, mean_field_log_R_q: {mean_field_log_R_q}, v_n: {v_n}, v_d: {v_d}")
+        logger.debug(f"error: {log_R_q - mean_field_log_R_q}")
+
+    np.savez(
+        file=output_dir / "results.npz",
+        q=np.array(q_ids),
+        R_numerators=np.array(R_numerators),
+        R_denominators=np.array(R_denominators),
+        log_Rs=np.array(log_Rs),
+        mean_field_log_Rs=np.array(mean_field_log_Rs),
+        U_nodes=np.array(list(unlabeled_nodes)),
+        Q_nodes=np.array(list(Q_nodes)),
+        L_nodes=np.array(list(labeled_nodes)),
+    )
+
+
+def _calculate_log_R_constant_term(*, labels, p_inter, p_intra, K, Q_nodes, L_nodes, edges):
+    """compute the constant term in log R"""
+    ret = 0
+    for i in L_nodes:
+        ret += _calculate_log_R_by_mean_field_approximation(
+            labels=labels,
+            p_inter=p_inter,
+            p_intra=p_intra,
+            K=K,
+            Q_nodes=Q_nodes,
+            i=i,
+            edges=edges,
+        )
+    return ret
+
+
+def _calculate_log_R_by_mean_field_approximation(*, labels, p_inter, p_intra, K, Q_nodes, i, edges):
+    p = p_intra
+    q = p_inter
+
+    log_p = np.log(p)
+    log_1mp = np.log(1 - p)
+    log_q = np.log(q)
+    log_1mq = np.log(1 - q)
+    expected_pq = np.log((p + (K - 1) * q) / K)
+    expected_1mpq = np.log(((1 - p) + (K - 1) * (1 - q)) / K)
+
+    # edges: Int[Tensor, "e 2"] = dataset.base.edge_indices.clone().cpu().T.tolist()
+    edge_set = set(tuple(e) for e in edges) | set(tuple(e) for e in edges[::-1])
+    u_label = labels[i]
+    a_same = 0  # connected and same label
+    a_diff = 0  # connected and different label
+    b_same = 0  # not connected and same label
+    b_diff = 0  # not connected and different label
+    for v in Q_nodes:
+        v_label = labels[v]
+        has_edge = (i, v) in edge_set
+        if v_label == u_label:
+            if has_edge:
+                a_same += 1
+            else:
+                b_same += 1
+        else:
+            if has_edge:
+                a_diff += 1
+            else:
+                b_diff += 1
+
+    cp_value = (
+        a_same * log_p
+        + b_same * log_1mp
+        + a_diff * log_q
+        + b_diff * log_1mq
+        - (a_same + a_diff) * expected_pq
+        - (b_same + b_diff) * expected_1mpq
+    )
+    return cp_value
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except Exception as e:
+        logger.error(traceback.format_exc())
+        raise e
+
+    logger.info("finished")
---
+++ src/plot/mean_field_error.py
@@ -0,0 +1,221 @@
+from enum import StrEnum
+from pathlib import Path
+
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib import colors
+
+from plot.constant import FIG_DIR
+
+# Constants
+result_dir = Path("/home/members/ono/workspace/cp4bal/out/mean_field_error/")
+exp_name = "csbm_n20_k2_snr2.0_d4_seed42"
+EXP_DIR = result_dir / exp_name
+
+
+class NodeLabel(StrEnum):
+    LABELED = "L"
+    QUERY = "Q"
+    UNLABELED = ""
+
+
+def main():
+    fig_dir = FIG_DIR / "mean_field_error"
+    fig_dir.mkdir(parents=True, exist_ok=True)
+
+    result_dict = np.load(EXP_DIR / "results.npz")
+
+    plot_scatter(
+        log_Rs=result_dict["log_Rs"],
+        mean_field_log_Rs=result_dict["mean_field_log_Rs"],
+        fig_dir=fig_dir,
+    )
+
+    plot_graph_with_error(
+        L_nodes=result_dict["L_nodes"],
+        U_nodes=result_dict["U_nodes"],
+        Q_nodes=result_dict["Q_nodes"],
+        edges=np.load(EXP_DIR / "edge_indices.npy").T,
+        gt_labels=np.load(EXP_DIR / "gt_labels.npy"),
+        errors=result_dict["log_Rs"] - result_dict["mean_field_log_Rs"],
+        fig_dir=fig_dir,
+    )
+
+    plot_graph_with_exact_logR(
+        L_nodes=result_dict["L_nodes"],
+        U_nodes=result_dict["U_nodes"],
+        Q_nodes=result_dict["Q_nodes"],
+        edges=np.load(EXP_DIR / "edge_indices.npy").T,
+        gt_labels=np.load(EXP_DIR / "gt_labels.npy"),
+        log_Rs=result_dict["log_Rs"],
+        fig_dir=fig_dir,
+    )
+
+
+def plot_scatter(log_Rs, mean_field_log_Rs, fig_dir):
+    fig, ax = plt.subplots()
+    ax.scatter(log_Rs, mean_field_log_Rs, marker="o", s=49, alpha=1.0, color="blue", edgecolors="black", linewidths=0.5)
+    ax.set_xlabel("Exact log R")
+    ax.set_ylabel("Approximate log R")
+    lims = [
+        np.min([ax.get_xlim(), ax.get_ylim()]),
+        np.max([ax.get_xlim(), ax.get_ylim()]),
+    ]
+    ax.plot(lims, lims, color="black", linestyle="-", alpha=0.75, zorder=0)
+    ax.set_aspect("equal")
+    ax.set_xlim(lims)
+    ax.set_ylim(lims)
+
+    fig.savefig(fig_dir / f"{exp_name}_corr.png", dpi=300)
+
+
+def plot_graph_with_error(L_nodes, U_nodes, Q_nodes, edges, gt_labels, errors, fig_dir):
+    import networkx as nx
+
+    G = nx.Graph()
+    G.add_edges_from(edges)
+    G.add_nodes_from(range(len(gt_labels)))
+
+    node_class_0 = [i for i, label in enumerate(gt_labels) if label == 0]
+    node_class_1 = [i for i, label in enumerate(gt_labels) if label == 1]
+    abs_err_max = np.max(np.abs(errors))
+    errors_all = np.zeros(len(gt_labels))
+    for i, n in enumerate(U_nodes):
+        errors_all[n] = errors[i]
+
+    for n in Q_nodes:
+        errors_all[n] = 0.0
+
+    node_errors_0 = [abs(errors_all[n]) for n in node_class_0]
+    node_errors_1 = [abs(errors_all[n]) for n in node_class_1]
+
+    pos = nx.spring_layout(G, seed=42, k=0.5)
+
+    plt.figure(figsize=(8, 8))
+    node_labels = {}
+    for node in G.nodes():
+        if node in L_nodes:
+            node_labels[node] = NodeLabel.LABELED.value
+        elif node in Q_nodes:
+            node_labels[node] = NodeLabel.QUERY.value
+        else:
+            node_labels[node] = NodeLabel.UNLABELED.value
+
+    cmap = plt.cm.OrRd
+    vmin = 0.0
+    vmax = abs_err_max
+    nx.draw_networkx_nodes(
+        G,
+        pos,
+        nodelist=node_class_0,
+        node_color=node_errors_0,
+        node_shape="o",
+        edgecolors="black",
+        cmap=cmap,
+        vmin=vmin,
+        vmax=vmax,
+        node_size=300,
+    )
+    nx.draw_networkx_nodes(
+        G,
+        pos,
+        nodelist=node_class_1,
+        node_color=node_errors_1,
+        edgecolors="black",
+        node_shape="s",
+        cmap=cmap,
+        vmin=vmin,
+        vmax=vmax,
+        node_size=300,
+    )
+    nx.draw_networkx_edges(G, pos)
+    nx.draw_networkx_labels(G, pos, labels=node_labels, font_size=12, font_color="black")
+    sm = plt.cm.ScalarMappable(cmap=cmap, norm=colors.Normalize(vmin=vmin, vmax=vmax))
+    sm.set_array([])
+    ax = plt.gca()
+    cbar = plt.colorbar(sm, ax=ax)
+    cbar.set_label("Abs Err", fontsize=18)
+    cbar.ax.tick_params(labelsize=14)
+    ax.set_axis_off()
+    ax.set_aspect("equal")
+    plt.tight_layout()
+    plt.savefig(fig_dir / f"{exp_name}_graph_error.png", dpi=300)
+
+
+def plot_graph_with_exact_logR(L_nodes, U_nodes, Q_nodes, edges, gt_labels, log_Rs, fig_dir):
+    import networkx as nx
+
+    G = nx.Graph()
+    G.add_edges_from(edges)
+    G.add_nodes_from(range(len(gt_labels)))
+
+    node_class_0 = [i for i, label in enumerate(gt_labels) if label == 0]
+    node_class_1 = [i for i, label in enumerate(gt_labels) if label == 1]
+    vmin = np.min(log_Rs)
+    vmax = np.max(log_Rs)
+    log_R_all = np.zeros(len(gt_labels))
+    for i, n in enumerate(U_nodes):
+        log_R_all[n] = log_Rs[i]
+
+    for n in Q_nodes:
+        log_R_all[n] = 0.0
+
+    node_values_0 = [log_R_all[n] for n in node_class_0]
+    node_values_1 = [log_R_all[n] for n in node_class_1]
+
+    pos = nx.spring_layout(G, seed=42, k=0.5)
+
+    plt.figure(figsize=(8, 8))
+    node_labels = {}
+    for node in G.nodes():
+        if node in L_nodes:
+            node_labels[node] = NodeLabel.LABELED.value
+        elif node in Q_nodes:
+            node_labels[node] = NodeLabel.QUERY.value
+        else:
+            node_labels[node] = NodeLabel.UNLABELED.value
+
+    if vmax > 0 > vmin:
+        norm = colors.TwoSlopeNorm(vmin=vmin, vcenter=0, vmax=vmax)
+    elif vmin >= 0:
+        norm = colors.TwoSlopeNorm(vmin=-vmax, vcenter=0, vmax=vmax)
+    else:
+        norm = colors.TwoSlopeNorm(vmin=vmin, vcenter=0, vmax=-vmin)
+    cmap = plt.cm.RdBu
+    node_colors_0 = [cmap(norm(v)) for v in node_values_0]
+    node_colors_1 = [cmap(norm(v)) for v in node_values_1]
+
+    nx.draw_networkx_nodes(
+        G,
+        pos,
+        nodelist=node_class_0,
+        node_color=node_colors_0,
+        node_shape="o",
+        edgecolors="black",
+        node_size=300,
+    )
+    nx.draw_networkx_nodes(
+        G,
+        pos,
+        nodelist=node_class_1,
+        node_color=node_colors_1,
+        edgecolors="black",
+        node_shape="s",
+        node_size=300,
+    )
+    nx.draw_networkx_edges(G, pos)
+    nx.draw_networkx_labels(G, pos, labels=node_labels, font_size=12, font_color="black")
+    sm = plt.cm.ScalarMappable(cmap=cmap, norm=norm)
+    sm.set_array([])
+    ax = plt.gca()
+    cbar = plt.colorbar(sm, ax=ax)
+    cbar.set_label("exact log R", fontsize=18)
+    cbar.ax.tick_params(labelsize=14)
+    ax.set_axis_off()
+    ax.set_aspect("equal")
+    plt.tight_layout()
+    plt.savefig(fig_dir / f"{exp_name}_graph_logR.png", dpi=300)
+
+
+if __name__ == "__main__":
+    main()
