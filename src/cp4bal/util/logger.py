e6f53e2aacff3e975b43dd3b3ed3df6b2f109741
Yuta Ono <ono-yuta116@g.ecc.u-tokyo.ac.jp>
2025-12-20 03:55:11 +0000

    add: logger for terminal only

--- src/cp4bal/util/logger.py
+++ src/cp4bal/util/logger.py
@@ -5,6 +5,7 @@ from pathlib import Path
 import yaml
 
 DEFAULT_LOGGER_CONFIG = Path(__file__).parent / "logger_config.yaml"
+CONSOLE_LOGGER_CONFIG = Path(__file__).parent / "logger_config_console.yaml"
 
 
 def init_logger(log_dir: Path, log_file_name: str, config_path: Path = DEFAULT_LOGGER_CONFIG) -> None:
@@ -13,6 +14,15 @@ def init_logger(log_dir: Path, log_file_name: str, config_path: Path = DEFAULT_L
         log_path = log_dir / f"{log_file_name}.log"
         log_path.parent.mkdir(parents=True, exist_ok=True)
         config["handlers"]["file"]["filename"] = str(log_path)
+        dictConfig(config)
+    getLogger("matplotlib").setLevel(WARNING)  # surpress unnecessary logging
+    getLogger("sklearn").setLevel(ERROR)  # surpress unnecessary logging
+    getLogger("numba").setLevel(WARNING)  # surpress unnecessary logging
+
+
+def init_stream_logger(config_path: Path = CONSOLE_LOGGER_CONFIG) -> None:
+    with config_path.open(mode="r") as f:
+        config = yaml.safe_load(f)
         dictConfig(config)
     getLogger("matplotlib").setLevel(WARNING)  # surpress unnecessary logging
     getLogger("sklearn").setLevel(ERROR)  # surpress unnecessary logging
